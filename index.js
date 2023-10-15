a7f23f3fe056f7a1d0493089cb98571b0d2711f3
Sergio Vargas <sheshovs@gmail.com>
2023-10-14 23:09:09 -0300

    first commit and mercado pago added

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express"
+import cors from "cors"
+import axios from "axios";
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+
+app.post("/api/mercadopago/create_preference", async (req, res) => {
+
+  const mercadoPagoUrl = `https://api.mercadopago.com/checkout/preferences?access_token=TEST-5849597827836358-101417-23c615c14370e68bfa7a84053ea4e2c2-62109829`
+
+  const preferenceData = {
+		items: req.body.items,
+    auto_return: "approved",
+		back_urls: {
+			"success": "https://aq517.netlify.app/",
+			"failure": "https://aq517.netlify.app/",
+			"pending": "https://aq517.netlify.app/"
+		},
+	}
+
+  try {
+    const response = await axios.post(mercadoPagoUrl, preferenceData, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    res.status(200).json({
+      url: response.data.init_point,
+    })
+  } catch (error) {
+    res.status(400).json(error)
+    console.log(error)
+  }
+
+});
+
+
+app.get('/api/mercadopago/feedback', function (req, res) {
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id
+	});
+});
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(4000, () => {
+    console.log("Server is listening on port 4000")
+})
